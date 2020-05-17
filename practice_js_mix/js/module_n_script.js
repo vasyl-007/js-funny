800fba05b2000d6c67a4ae1198e30d9207bf2615
Vasyl-007 <vasyl.manyo@gmail.com>
2020-05-17 23:23:59 +0300

    d

--- practice_js_mix/js/module_n_script.js
+++ practice_js_mix/js/module_n_script.js
@@ -1913,7 +1913,16 @@
 // With the isNaN method, you can check if the value you pass is not a number. name is not a number, so isNaN(name) returns true. age is a number, so isNaN(age) returns false.
 
 // =================== test 129 =========================
+// const randomValue = 21;
 
+// function getInfo() {
+//   console.log(typeof randomValue);
+//   const randomValue = "Lydia Hallie";
+// }
+
+// getInfo();
+
+// Variables declared with the const keyword are not referencable before their initialization: this is called the temporal dead zone. In the getInfo function, the variable randomValue is scoped in the functional scope of getInfo. On the line where we want to log the value of typeof randomValue, the variable randomValue isn't initialized yet: a ReferenceError gets thrown! The engine didn't go down the scope chain since we declared the variable randomValue in the getInfo function.
 
 // =================== test 130 =========================
 // =================== test 131 =========================
