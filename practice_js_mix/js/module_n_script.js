2dc00ed49fd7d06df5ec204e37c588dab62f894f
Vasyl-007 <vasyl.manyo@gmail.com>
2020-05-17 23:22:32 +0300

    d

--- practice_js_mix/js/module_n_script.js
+++ practice_js_mix/js/module_n_script.js
@@ -1898,10 +1898,23 @@
 
 // By destructuring objects, we can unpack values from the right-hand object, and assign the unpacked value to the value of the same property name on the left-hand object. In this case, we're assigning the value "💀" to spookyItems[3]. This means that we're modifying the spookyItems array, we're adding the "💀" to it. When logging spookyItems, ["👻", "🎃", "🕸", "💀"] gets logged.
 
-
-
 // =================== test 128 =========================
+// const name = "Lydia Hallie";
+// const age = 21;
+
+// console.log(Number.isNaN(name));    // false
+// console.log(Number.isNaN(age));     // false    
+
+// console.log(isNaN(name));           // true
+// console.log(isNaN(age));            // false
+
+// With the Number.isNaN method, you can check if the value you pass is a numeric value and equal to NaN. name is not a numeric value, so Number.isNaN(name) returns false. age is a numeric value, but is not equal to NaN, so Number.isNaN(age) returns false.
+
+// With the isNaN method, you can check if the value you pass is not a number. name is not a number, so isNaN(name) returns true. age is a number, so isNaN(age) returns false.
+
 // =================== test 129 =========================
+
+
 // =================== test 130 =========================
 // =================== test 131 =========================
 // =================== test 132 =========================
