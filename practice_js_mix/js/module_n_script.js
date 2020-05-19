482863234fdc238cad43b92718c3b7d349df921e
Vasyl-007 <vasyl.manyo@gmail.com>
2020-05-19 18:14:43 +0300

    d

--- practice_js_mix/js/module_n_script.js
+++ practice_js_mix/js/module_n_script.js
@@ -1999,7 +1999,7 @@
 //   console.log(await res);
 //   setTimeout(() => console.log("Timeout!", 0));
 //   console.log("Last line!");
-}
+// }
 
 // funcOne();    // Last line!   Promise!  Timeout! 0  
 // funcTwo();    // Promise!  Last line!   Timeout! 0   
@@ -2049,6 +2049,24 @@
 // We can invoke this function, by calling sum.default
 
 // =================== test 135 =========================
+// const handler = {
+// 	set: () => console.log("Added a new property!"),
+// 	get: () => console.log("Accessed a property!")
+// };
+
+// let person = new Proxy({}, handler);
+
+// // person.name = "Lydia";   // Added a new property!
+// person.name;          // Accessed a property!
+
+// With a Proxy object, we can add custom behavior to an object that we pass to it as the second argument. In tis case, we pass the handler object which contained to properties: set and get. set gets invoked whenever we set property values, get gets invoked whenever we get (access) property values.
+
+// The first argument is an empty object {}, which is the value of person. To this object, the custom behavior specified in the handler object gets added. If we add a property to the person object, set will get invoked. If we access a property on the person object, get gets invoked.
+
+// First, we added a new property name to the proxy object (person.name = "Lydia"). set gets invoked, and logs "Added a new property!".
+
+// Then, we access a property value on the proxy object, the get property on the handler object got invoked. "Accessed a property!" gets logged.
+
 // =================== test 136 =========================
 // =================== test 137 =========================
 // =================== test 138 =========================
